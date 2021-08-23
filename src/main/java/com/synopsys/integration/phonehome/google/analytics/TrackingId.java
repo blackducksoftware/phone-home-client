1020fbbf006c2830bdf0bf92310ac062db4d7554
Used to build SIG applications <serv-builder@int-ubuntu-jenkins02.dc1.lan>
2021-08-23 15:29:58 -0400

    Using the next snapshot post release 5.1.1-SNAPSHOT

--- src/main/java/com/synopsys/integration/phonehome/google/analytics/TrackingId.java
+++ src/main/java/com/synopsys/integration/phonehome/google/analytics/TrackingId.java
@@ -1,3 +1,10 @@
+/*
+ * phone-home-client
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.phonehome.google.analytics;
 
 public class TrackingId {
